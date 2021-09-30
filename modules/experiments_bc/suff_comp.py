25dcf09ab97d0739d9d8e1cb18840b1c4db8fb3c
George Chrysostomou <acp18gdc@dcs30614.comsciad.dcs.shef.ac.uk>
2021-09-30 12:22:46 +0100

    DEVEL

---
+++ modules/experiments_bc/suff_comp.py
@@ -0,0 +1,246 @@
+import torch
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+import pandas as pd
+from modules.experiments_bc.eval_utils import * 
+from collections import OrderedDict
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+from tqdm import trange
+import json
+from modules.utils import normalized_comprehensiveness_, normalized_sufficiency_, sufficiency_, create_rationale_mask_
+
+with open('modules/config.txt', 'r') as f:
+    args = json.load(f)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def conduct_tests_(data, model, save_path):
+
+    pbar = trange(len(data) * data.batch_size, desc="running for AOPC", leave=True)
+    
+    faithfulness_results = {}
+
+    if args["encoder"] == "bert":
+        model.encoder.bert.embeddings.requires_grad_(True)
+
+    else:
+        model.encoder.embedding.weight.requires_grad_(True)
+
+    counter = 0
+
+    for sentences, lengths, labels in data:
+        
+        torch.cuda.empty_cache()
+        model.zero_grad()
+        model.eval()
+
+        if args["encoder"] == "bert":
+            
+            sentences, lengths, labels = torch.stack(sentences).transpose(0,1).to(device), lengths.to(device), labels.to(device)
+                
+        else:
+            
+            sentences, lengths, labels = Variable(sentences).to(device),Variable(lengths).to(device), Variable(labels).to(device)
+
+        index_list = []
+
+        for _i_ in range(sentences.size(0)):
+            
+            index = f"test-{counter}"
+
+            faithfulness_results.update({index:{}})
+            
+            counter+=1
+            index_list.append(index)
+
+        original_prediction, weights_or = model(
+            sentences, 
+            lengths, 
+            retain_gradient = True
+        )
+
+        original_prediction.max(-1)[0].sum().backward(retain_graph = True)
+
+        g = model.encoder.embed.grad
+
+        em = model.encoder.embed
+
+        g1 = (g* em).sum(-1)[:,:max(lengths)]
+
+        integrated_grads = model.integrated_grads(
+            sentences, 
+            g, 
+            lengths, 
+            original_pred = original_prediction.max(-1)
+        )
+         
+        weights_def_grad = model.weights.grad
+
+        random = torch.randn(weights_or.shape)
+
+        g1.masked_fill_(model.masks[:,:max(lengths)].bool(), float("-inf"))
+
+
+        omission_scores = model.get_omission_scores(
+            sentences, 
+            lengths, 
+            original_prediction
+        )
+
+        integrated_grads.masked_fill_(model.masks[:,:max(lengths)].bool(), float("-inf"))
+
+        weight_mul_grad = weights_or * weights_def_grad
+        
+        weights_def_grad.masked_fill_(model.masks[:,:max(lengths)].bool(), float("-inf"))      
+        
+        weight_mul_grad.masked_fill_(model.masks[:,:max(lengths)].bool(),float("-inf"))
+
+        random.masked_fill_(model.masks[:,:max(lengths)].bool(),float("-inf"))
+
+
+        original_sentences = sentences.clone().detach()[:,:max(lengths)]
+
+        model.eval()
+
+
+        """"""
+
+        original_prediction = torch.softmax(original_prediction, dim = -1).detach().cpu().numpy()
+        full_text_class = original_prediction.argmax(-1)
+        full_text_probs = original_prediction.max(-1)
+
+        sentences = original_sentences* torch.zeros_like(original_sentences).to(device)
+
+        yhat, _  = model(
+            original_sentences, 
+            lengths,
+            retain_gradient = True,
+            ig = 0.
+        )
+
+        yhat = torch.softmax(yhat, dim = -1).detach().cpu().numpy()
+
+        rows = torch.arange(sentences.size(0)).long().to(device)
+
+        reduced_probs = yhat[rows, full_text_class]
+
+        ## baseline sufficiency
+        suff_y_zero = sufficiency_(
+            full_text_probs, 
+            reduced_probs
+        )
+
+        ## AOPC scores and other metrics
+        rationale_ratios = [0.05, 0.1, 0.2, 0.5]
+
+        for rationale_type in {"topk"}:
+
+            for _j_, annot_id in enumerate(index_list):
+                    
+                faithfulness_results[annot_id]["full text prediction"] = original_prediction[_j_] 
+                faithfulness_results[annot_id]["true label"] = labels[_j_].detach().cpu().item()
+            
+            for feat_name, feat_score in {"random" : random, "attention" : weights_or, "gradients" : g1, 
+                                            "ig" : integrated_grads, "omission" : omission_scores , 
+                                            "scaled attention" : weight_mul_grad, "attention gradients" : weights_def_grad}.items():
+
+            # for feat_name, feat_score in {"gradients" : g1, 
+            #                                 "ig" : integrated_grads}.items():
+
+                suff_aopc = np.zeros([yhat.shape[0], len(rationale_ratios)], dtype=np.float64)
+                comp_aopc = np.zeros([yhat.shape[0], len(rationale_ratios)], dtype=np.float64)
+
+                for _i_, rationale_length in enumerate(rationale_ratios):
+                    
+                    ## if we are masking for a query that means we are preserving
+                    ## the query and we DO NOT mask it
+                  
+                    rationale_mask = create_rationale_mask_(
+                        importance_scores = feat_score, 
+                        no_of_masked_tokens = torch.ceil(lengths.float() * rationale_length).detach().cpu().numpy(),
+                        method = rationale_type
+                    )
+
+                    ## measuring faithfulness
+                    comp, _  = normalized_comprehensiveness_(
+                        model = model, 
+                        original_sentences = original_sentences, 
+                        rationale_mask = rationale_mask, 
+                        inputs = {"input" : original_sentences, "lengths":lengths}, 
+                        full_text_probs = full_text_probs, 
+                        full_text_class = full_text_class, 
+                        rows = rows,
+                        suff_y_zero = suff_y_zero
+                    )
+
+                    suff, _ = normalized_sufficiency_(
+                        model = model, 
+                        original_sentences = original_sentences, 
+                        rationale_mask = rationale_mask, 
+                        inputs = {"input" : original_sentences, "lengths":lengths}, 
+                        full_text_probs = full_text_probs, 
+                        full_text_class = full_text_class, 
+                        rows = rows,
+                        suff_y_zero = suff_y_zero
+                    )
+
+                    suff_aopc[:,_i_] = suff
+                    comp_aopc[:,_i_] = comp
+
+                    # import pdb; pdb.set_trace()
+                    
+                for _j_, annot_id in enumerate(index_list):
+                    
+                    faithfulness_results[annot_id][feat_name] = {
+                        "sufficiency aopc" : {
+                            "mean" : suff_aopc[_j_].sum() / (len(rationale_ratios) + 1),
+                            "per ratio" : suff_aopc[_j_]
+                        },
+                        "comprehensiveness aopc" : {
+                            "mean" : comp_aopc[_j_].sum() / (len(rationale_ratios) + 1),
+                            "per ratio" : comp_aopc[_j_]
+                        }
+                    }
+        
+
+            
+           
+        pbar.update(data.batch_size)
+
+            
+    descriptor = {}
+    # filling getting averages
+    for feat_attr in {"attention", "gradients", "ig", "random", "scaled attention", "attention gradients", "omission"}:
+        
+        aopc_suff= np.asarray([faithfulness_results[k][feat_attr][f"sufficiency aopc"]["mean"] for k in faithfulness_results.keys()])
+        aopc_comp = np.asarray([faithfulness_results[k][feat_attr][f"comprehensiveness aopc"]["mean"] for k in faithfulness_results.keys()])
+
+        descriptor[feat_attr] = {
+            "AOPC - sufficiency" : {
+                "mean" : aopc_suff.mean(),
+                "std" : aopc_suff.std()
+            },
+            "AOPC - comprehensiveness" : {
+                "mean" : aopc_comp.mean(),
+                "std" : aopc_comp.std()
+            }
+        }
+
+    ## save all info
+    fname = save_path + "faithfulness-scores-detailed-.npy"
+
+    np.save(fname, faithfulness_results)
+
+    ## save descriptors
+    fname = save_path + "faithfulness-scores-averages--description.json"
+
+    with open(fname, 'w') as file:
+            json.dump(
+                descriptor,
+                file,
+                indent = 4
+            ) 
+
+    return
